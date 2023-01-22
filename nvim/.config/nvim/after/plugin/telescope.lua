f890115de5ef560d51e68d002356e3cb294105dd
JVA <nafets04@gmail.com>
2023-01-22 20:05:06 +0100

    Optimized remap.lua

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,27 @@
 local builtin = require("telescope.builtin")
 
+local telescope = require('telescope')
+telescope.setup {
+    defaults = {
+        file_ignore_patterns = {
+            "miniconda3", "venv"
+        },
+    },
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    }
+}
+
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fw", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>fl', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fc', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+
+return M
+
+--vim.keymap.set("n", "<leader>fw", function()
+--    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+--end)
--- nvim/.config/nvim/lua/jva/remap.lua
+++ nvim/.config/nvim/lua/jva/remap.lua
@@ -1,2 +1,35 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-- move lines in Block Mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- stay in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste and delete without losing current paste buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("n", "<leader>d", "\"_dP")
+vim.keymap.set("v", "<leader>d", "\"_dP")
+
+-- yank to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- quick fix list
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- replace word under the cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
